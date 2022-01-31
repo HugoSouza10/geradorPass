c3a4b2e38ccd51b16258dad7f755ab9e3911f58d
HugoSouza <51915862+HugoSouza10@users.noreply.github.com>
2022-01-31 20:25:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-//ESCOPO GLOBAL
+
 let sizeInput = document.querySelector('#size');
 let displayRange = document.querySelector('#displayGerator');
 let result = document.querySelector('.result');
@@ -10,15 +10,11 @@ let optionGerator = document.querySelector('#optionGerator');
 let checkSelected = document.querySelectorAll('.boxGeration input[type="checkbox"]');
 
 console.log(checkSelected);
-//GUARDA O VALOR ATUAL
-let valueCurrent; 
- //VARIAVÉL PARA ARMAZENAR A NOVA SENHA
+ let valueCurrent; 
  let newPass;
- //VARIÁVEL PARA GERAR SENHA FÁCIL, PEGAMOS O VALOR MINÍMO 
  let memorizesEasy = sizeInput.min;
 
 
- //POSSIBILIDADE DE SENHA
 
  let arrayPoss = [
    'A', 'B', 'C', 'D','E',
@@ -36,20 +32,17 @@ let valueCurrent;
 
 
 
-//FUNÇÃO COPIAR 
 let copyButtun = document.querySelector('#copy');
 
 
 copyButtun.addEventListener('click',()=>{
      let copyPass = result.innerHTML;
-     //COMANDO PARA COPIAR A SENHA
      navigator.clipboard.writeText(copyPass);
      alert('Copiado com sucesso!')
 })
 
 
 
-//FUNÇÃO PARA ESCOLHER A OPÇÃO DO GERADOR
 
 optionGerator.addEventListener('change',(event)=>{
       let opPass = parseInt(event.target.value);
@@ -58,18 +51,16 @@ optionGerator.addEventListener('change',(event)=>{
 
       switch (opPass) {
          case 1:
-            //LIMPANDO A TELA
             result.innerHTML = '';
-            valueCurrent = valueDefault;  //ATUALIZANDO O VALOR ATUAL COM O VALOR PADRÃO
-            displayRange.innerHTML = valueCurrent; //MOSTRANDO O VALOR NA TELA DO RANGE
+            valueCurrent = valueDefault;  
+            displayRange.innerHTML = valueCurrent; 
             renderPass(valueDefault);
             break;
 
          case 2:
             result.innerHTML = '';
-            valueCurrent = memorizesEasy; //ATUALIZANDO O VALOR ATUAL PARA A SENHA ALEATÓRIA FÁCIL
+            valueCurrent = memorizesEasy;
             renderPass(memorizesEasy);
-            //ATUALIZANDO O DISPLAY DO CHANGE
             displayRange.innerHTML = memorizesEasy;
             break;   
       
@@ -82,12 +73,9 @@ optionGerator.addEventListener('change',(event)=>{
 
 
 
-//VERIFICANDO SE ALGUÉM SELECIONOU O CHECKBOX PARA REMOVER NÚMERO OU SÍMBOLOS
-
-//COMO O CHECK É UM ARRAY DO SELECTOR ALL. ENTÃO A GENTE PRECISA FAZER UM MAP OU FOR EACH PARA PODER PEGAR OS ITENS E ADICIONAR EVENTOS
 checkSelected.forEach((item)=>{
      item.addEventListener('change',()=>{
-        if(item.checked == true){ //VERIFICANDO SE O VALOR DO ITEM ENCONTRADO NO LOOP É IGUAL A TRUE, POIS SIGNIGICA QUE ELE TÁ SELECIOANDO
+        if(item.checked == true){ 
            if(item.value == 'removeNumeros'){ 
 
                for(let i=0; i<arrayPoss.length; i++){
@@ -106,30 +94,20 @@ checkSelected.forEach((item)=>{
 
 
 
-
-
-
-//DEFININDO VALOR PADRÃO DO INPUT GERADOR AO INCIAR
-
 let defaultUpdate = window.onload = ()=>{
 
-   
-      //FUNÇÃO ATUALIZAR SENHA
+  
       update.addEventListener('click',()=>{
-         //LIMPANDO A TELA 
          result.innerHTML = '';
          defaultUpdate();
          
       });
 
 
-
-      //VERIFICA SE EXISTE UM VALOR RECENTE
       if(valueCurrent>0){
          renderPass(valueCurrent);
          displayRange.innerHTML = valueCurrent;
       }else{
-         //SE NÃO EXISTIR UM VALOR RECENTE, ENTÃO PEGAMOS O VALOR GERADO PELO O INPUT OU PADRÃO
          displayRange.innerHTML = sizeInput.value;
          renderPass(valueDefault);
       }
@@ -138,9 +116,6 @@ let defaultUpdate = window.onload = ()=>{
 }
 
 
-
-
-//FUNÇÃO LIMPAR, A GENTE MANDA O VALUE, POIS É O RESPONSÁVEL PELO O LOOP
 function clearPass(value){
    for(let i = 0; i<value; i++){
       newPass = '';
@@ -150,9 +125,7 @@ function clearPass(value){
 }
 
 
-//FUNÇÃO PARA DEFINIR O VALOR DO RANGE
 function rangeInput(value){
-   //TROCANDO O VALOR PADRÃO DO VALOR ATUAL
    valueCurrent = value;
    displayRange.innerHTML = value;
    renderPass(sizeInput.value);
@@ -162,12 +135,10 @@ function rangeInput(value){
 //FUNÇÃO GERAR SENHA 
 
 function renderPass(value){
-    //GERANDO SENHA PADRÃO COM LIMITE PADRÃO TRAVADO
     if(value == valueDefault ){
        generateDefault(value, arrayPoss);
 
     }else{
-      //GERANDO SENHA COM O CAMPO RANGE
        if(value>valueDefault || value<valueDefault){
          clearPass(value);
           result.innerHTML = newPass;
@@ -179,38 +150,29 @@ function renderPass(value){
 
 
 
-/*FUNÇÃO GERAR SENHA PADRÃO */
 
 function generateDefault(value, arrayPoss){
-    //VARIAVÉL PARA ALTERNAR A LETRA
     let randomLetter = false;
 
    for(let i = 0; i<value; i++){
-      //VARIÁVEL PARA GUARDAR UM ITEM ALEATÓRIO
       let randomOne = '';
      
       clearPass(value);
-        //GERANDO NÚMERO ALEATÓRIO PARA GERAR SENHA
       let numberRandomPoss = Math.floor(Math.random() * arrayPoss.length);
 
 
       if(randomLetter == true){
-         //PEGANDO UM ITEM DO ARRAY DE FORMA ALETATÓRIA
          randomOne = arrayPoss[numberRandomPoss];
          newPass+=randomOne;
-         //MOSTRANDO NA TELA
          result.append(newPass);
          randomLetter = false;
       }else{
          randomOne = arrayPoss[numberRandomPoss];
-         //TRANFORMANDO EM MINUSCULA
          newPass+=randomOne.toLocaleLowerCase();
-         //MOSTRANDO NA TELA
          result.append(newPass);
          randomLetter = true;
       }
 
-      //ARMAZENANDO O VALOR ATUAL
       valueCurrent = value;
 
      
@@ -222,37 +184,26 @@ function generateDefault(value, arrayPoss){
 
 
 
-
-
-/*FUNÇÃO GERAR SENHA ALEATÓRIA */
-
 function generateRandom(value, arrayPoss){
 
-   //VARIAVÉL PARA ALTERNAR A LETRA
    let randomLetter = true;
 
    console.log(value);
 
    for(let i = 0; i<value; i++){
 
-      //VARIÁVEL PARA GUARDAR UM ITEM ALEATÓRIO
       let randomOne = '';
       let newPass = '';
-        //GERANDO NÚMERO ALEATÓRIO PARA GERAR SENHA
       let numberRandomPoss = Math.floor(Math.random() * arrayPoss.length);
 
       if(randomLetter == true){
-         //PEGANDO UM ITEM DO ARRAY DE FORMA ALETATÓRIA
          randomOne = arrayPoss[numberRandomPoss];
          newPass+=randomOne;
-         //MOSTRANDO NA TELA
          result.append(newPass);
          randomLetter = false;
       }else{
          randomOne = arrayPoss[numberRandomPoss];
-         //TRANFORMANDO EM MINUSCULA
          newPass+=randomOne.toLocaleLowerCase();
-         //MOSTRANDO NA TELA
          result.append(newPass);
          randomLetter = true;
       }
@@ -266,34 +217,4 @@ function generateRandom(value, arrayPoss){
 
 
 
-/*https://codepen.io/alexandrebatista83/pen/abdKyqQ */
 
-
-//MÉTODO JOIN: SERVE PARA PEGAR STRING DE UM ARRAY
-
-
-//FUNÇÃO COPIAR TEXTO https://programandosolucoes.dev.br/2021/06/08/copiar-texto-javascript/
-
-
-
-
-/*
-
-O QUE APRENDEMOS?
-
-
--LÓGICA
-- IF E ELSE
-- APPEND
-- ARRAY
-- FUNÇÕES
-- FUNÇÃO CLIPBORD (COPIAR CONTEÚDO);
-- LOOP DENTRO DE ARRAY
-
-
-VOU SER O MELHOR PROGRAMADOR DO MUNDO!!!
-
-NADA É IMPOSSÍVEL PRA QUEM QUER
-
-
-*/
